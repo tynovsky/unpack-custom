129372acaebde15e085d5fbc8bc1d0b97a09c0e2
Týnovský Miroslav <tynovsky@avast.com>
2014-11-24 11:15:46 -0500

    initial commit: what minilla created

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Unpack::Recursive - It's new $module
+
+# SYNOPSIS
+
+    use Unpack::Recursive;
+
+# DESCRIPTION
+
+Unpack::Recursive is ...
+
+# LICENSE
+
+Copyright (C) Týnovský Miroslav.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Týnovský Miroslav <tynovsky@avast.com>
---
+++ lib/Unpack/Recursive.pm
@@ -0,0 +1,39 @@
+package Unpack::Recursive;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Unpack::Recursive - It's new $module
+
+=head1 SYNOPSIS
+
+    use Unpack::Recursive;
+
+=head1 DESCRIPTION
+
+Unpack::Recursive is ...
+
+=head1 LICENSE
+
+Copyright (C) Týnovský Miroslav.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Týnovský Miroslav E<lt>tynovsky@avast.comE<gt>
+
+=cut
+
